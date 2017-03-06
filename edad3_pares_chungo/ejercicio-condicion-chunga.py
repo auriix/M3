a2ea75bd8590eb3ad63c48161af887d9a9808a31
auriix <albavidalfravega@gmail.com>
2017-03-06 12:01:04 +0100

    Add files via upload

---
+++ edad3_pares_chungo/ejercicio-condicion-chunga.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+numero = input ("Inserta un número : ")
+
+if numero %2 == 0 :
+	print "Número par."
+	
+if (numero >= -10 and numero <= 40) :
+	print "Número entre -10 y 40."
+
+if numero < 0 :
+	print "Número negativo."
