5d09f40d1787e19e90369edf1f5f8f39eb39a877
auriix <albavidalfravega@gmail.com>
2017-03-06 12:02:49 +0100

    Delete ejercicio-edad-3.py

--- ejercicio-edad-3.py
+++
@@ -1,7 +0,0 @@
-#coding: utf8
-
-edad = input ("Inserta tu edad: ")
-if (edad >= 18 and edad <=23) or (edad == 17) or (edad >= 65 and edad <= 72) :
-	print "Puedes entrar en la sesión de jóvenes."
-else:
-	print "No puedes entrar en la sesión de jóvenes."
