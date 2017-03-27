68f69fbec21e28948bfb5c2c4ad8494e8ebdba02
auriix <albavidalfravega@gmail.com>
2017-03-27 13:22:05 +0200

    Plantilla

---
+++ BUCLES/plantilla_bucle.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+
+salir = False
+
+while (salir == False) :
+	
+	print ("Hola")
+	
+	tecla = raw_input ("Sal con s o S:")
+	
+	if (tecla == "S" or tecla == "s" ) :
+  	print ("Adiós")
+    salir = True
