3b61d43db2d7c1c03e0bfeb28bded772cc311387
auriix <albavidalfravega@gmail.com>
2017-03-27 13:40:13 +0200

    bucle 3

---
+++ BUCLES/ejercicio-bucle-contar2.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+
+num = 1
+limite = raw_input ("Inserta el límite: ")
+salir = False
+
+while (salir == False) :
+	
+	print num
+	
+	if (num = limite) :
+		salir = True
+  
+  num = num + 1
