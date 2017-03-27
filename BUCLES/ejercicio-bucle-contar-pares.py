9b98f304886d6504d3794db108afd514c78e4e11
auriix <albavidalfravega@gmail.com>
2017-03-27 13:47:58 +0200

    bucle 4.1

---
+++ BUCLES/ejercicio-bucle-contar-pares.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+
+num = 2
+limite = raw_input ("Escribe el límite: "
+salir = False
+
+while (salir == False) :
+	
+  if (num%2 == 0) :
+  	print num
+	
+	if (num == limite) :
+		salir = True
+  
+  num = num + 1
