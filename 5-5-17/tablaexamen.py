97b31f3e1a7f16f75b08437c2f1731b41e8841f7
auriix <albavidalfravega@gmail.com>
2017-05-04 20:38:21 +0200

    Create tablaexamen.py

---
+++ 5-5-17/tablaexamen.py
@@ -0,0 +1,17 @@
+for fil in my_range(1,5,1):
+  for col in my_range(1,4,1):
+    if (fil==1):
+      if (col==2):
+        print "A"
+      if (col==3):
+        print "B"
+      if (col==4):
+        print "C"
+      
+      else:
+        print "-"
+    else:
+      if (col==1):
+        print fil-1
+        
+       
