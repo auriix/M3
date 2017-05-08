f4b3fe404b591d2a237c84bc988f8fbb1ceb999e
Auriix <auriix@localhost.localdomain>
2017-05-08 20:53:57 +0200

    1

--- forexamen.py
+++ forexamen.py
@@ -1,18 +1,33 @@
-for fil in xrange (1, 5, 1):
-  for col in xrange (1, 4, 1):
-    if (fil==1):
-      if (col==2):
-        print "A",
-      if (col==3):
-        print "B",
-      if (col==4):
-        print "C",
-      else:
-        print "-"
-    else:
-      if (col==1):
-        print fil -1,
-      else:
-        if (fil==3 and col==3) or (fil==4 and fil==2):
-          print "*"
-    print ""
+
+fil=0
+col=0
+
+def my_range(inici, fi, increment):
+    while inici <= fi:
+        #Retorna l'element actual del rang (llista)
+        yield inici
+        inici = inici + increment
+
+
+for fil in my_range (1,5,1):
+	print ""
+	for col in my_range (1,4,1):
+		if (fil==1):
+			if (col==1):
+				print "-",
+			if (col==2):
+				print "A",
+			if (col==3):
+				print "B",
+			if (col==4):
+				print "C",
+			
+		else:
+			if (col==1):
+				print fil -1,
+			else:
+				if (fil==3 and col==2) or (fil==4 and col==3):
+					print "*",
+				else:
+					print "-",
+	
