49233294a594734c26a0e96762a8537dc0ef38d1
Auriix <auriix@localhost.localdomain>
2017-05-10 22:50:00 +0200

    sense acabar

---
+++ 11-5-17/calendari.py
@@ -0,0 +1,21 @@
+import calendar
+
+cont = 1
+
+mes = raw_input ("Inserta el mes: ") 
+anyo = raw_input ("Inserta el año: ") 
+
+
+
+if tope == 
+
+def my_range(inici, fi, increment):
+    while inici <= fi:
+        #Retorna l'element actual del rang (llista)
+        yield inici
+        inici = inici + increment
+
+
+for fil in my_range (1,8,1):
+	for col in my_range (1,8,1):
+		if (fil==1)
