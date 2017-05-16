f5a286755c458c7e6813fff086b983f51f823364
auriix <albavidalfravega@gmail.com>
2017-05-16 16:53:41 +0200

    Update ejercicio-bucle-vuelta_derecha.py

--- Python_comentado/BUCLES/ejercicio-bucle-vuelta_derecha.py
+++ Python_comentado/BUCLES/ejercicio-bucle-vuelta_derecha.py
@@ -1,9 +1,13 @@
 #coding: utf-8
 
+#Inicialització de les variables.
 num = 1
 salir = False
 
+#Entra al bucle.
 while (salir==False) :
+	
+	#Condicions
 	if (num%8==1 or num%8==2) :
 		print "arriba"
 		
@@ -16,9 +20,11 @@ while (salir==False) :
 	if (num%8==7 or num%8==0) :
 		print "izquierda"
 		
+	#Condició de sortida del bucle.	
 	if (num==8) :
 		salir = True
 	
+	#Increment de la variable num.	
 	num = num + 1
 		
 		
