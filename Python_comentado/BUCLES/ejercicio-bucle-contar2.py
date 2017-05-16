6d362cd76b45577bf7f5ccea33cee95651793eec
auriix <albavidalfravega@gmail.com>
2017-05-16 17:37:29 +0200

    Update ejercicio-bucle-contar2.py

--- Python_comentado/BUCLES/ejercicio-bucle-contar2.py
+++ Python_comentado/BUCLES/ejercicio-bucle-contar2.py
@@ -1,13 +1,16 @@
 #coding: utf-8
 
+#Inicialització de les variables.
 num = 1
 limite = input ("Inserta el límite: ")
 salir = False
 
+#Entra al bucle.
 while (salir == False) :
 	print num
 	
+	#Condició de sortida, surt cuan arriba al limit escollit a la inicialització de variables.
 	if (num == limite) :
 		salir = True
-  
+  	#Increment de variable num.
 	num = num + 1
