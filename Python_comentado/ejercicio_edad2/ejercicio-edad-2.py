7584c372244d1a7326aca2fb3302ee86c689b82b
auriix <albavidalfravega@gmail.com>
2017-05-16 16:39:25 +0200

    Update ejercicio-edad-2.py

--- Python_comentado/ejercicio_edad2/ejercicio-edad-2.py
+++ Python_comentado/ejercicio_edad2/ejercicio-edad-2.py
@@ -3,8 +3,10 @@
 #Esperem a llegir una resposta numèrica, la associem a la variable "edad"
 edad = input ("Inserta tu edad: ")
 
-
+#Condició:
 if edad >= 18 and edad <=23 :
+	#Si es compleix la condició.
 	print "Puedes entrar en la sesión de jóvenes."
-else:
+else: 
+	#Si no es compleix la condició.
 	print "No puedes entrar en la sesión de jóvenes."
