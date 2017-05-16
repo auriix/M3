98d9adb3c28964395b24e78f16ae756d052f96a0
auriix <albavidalfravega@gmail.com>
2017-05-16 16:43:29 +0200

    Update ejercicio-edad-3.py

--- Python_comentado/edad3_pares_chungo/ejercicio-edad-3.py
+++ Python_comentado/edad3_pares_chungo/ejercicio-edad-3.py
@@ -1,7 +1,12 @@
 #coding: utf8
 
+#Esperem a llegir una resposta numèrica, la associem a la variable "edad"
 edad = input ("Inserta tu edad: ")
+
+#Condició:
 if (edad >= 18 and edad <=23) or (edad == 17) or (edad >= 65 and edad <= 72) :
+	#Si es compleix la condició.
 	print "Puedes entrar en la sesión de jóvenes."
 else:
+	#Si no es compleix la condició.
 	print "No puedes entrar en la sesión de jóvenes."
