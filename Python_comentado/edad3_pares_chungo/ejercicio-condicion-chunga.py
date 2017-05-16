c644416c7f36727733632ede1f4af75531d14cda
auriix <albavidalfravega@gmail.com>
2017-05-16 16:43:25 +0200

    Update ejercicio-condicion-chunga.py

--- Python_comentado/edad3_pares_chungo/ejercicio-condicion-chunga.py
+++ Python_comentado/edad3_pares_chungo/ejercicio-condicion-chunga.py
@@ -1,8 +1,12 @@
 # coding: utf-8
 
+#Esperem a llegir una resposta numèrica, la associem a la variable "numero"
 numero = input ("Inserta un número : ")
 
+#Condició:
 if (numero %2 == 0) and (numero >= -10 and numero <= 40) and (numero < 0):
+	#Si es compleix la condició.
 	print "Cumple las 3 condiciones."
 else:
+	#Si no es compleix la condició.
 	print "No cumple las 3 condiciones."
