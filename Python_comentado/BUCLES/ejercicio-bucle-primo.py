5b14943b1141000dd0eaa520534115c97a2f61ca
auriix <albavidalfravega@gmail.com>
2017-05-16 17:13:10 +0200

    Update ejercicio-bucle-primo.py

--- Python_comentado/BUCLES/ejercicio-bucle-primo.py
+++ Python_comentado/BUCLES/ejercicio-bucle-primo.py
@@ -1,17 +1,25 @@
  #coding: utf-8
- 
+	
+#Inicialització de les variables.
 num = input ("Inserta un número: ")
 cont = num
 ceros = 0
 salir = False 
- 
+
+#Entra al bucle.
 while (salir == False) :
+	
+#Condicions
+
 	if (num%cont == 0) :
+		#Increment de la variable 0.
 		ceros = ceros + 1
 		
+	#Condició de sortida.
 	if (cont == 1) :
 		salir = True
-  
+		
+  	#Disminució de la variable cont
 	cont = cont - 1
 
 if (ceros == 2) :
