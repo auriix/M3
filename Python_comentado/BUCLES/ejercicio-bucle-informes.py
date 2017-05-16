b68bf44555eb99ab0449c272087a9b51e13ab9ee
auriix <albavidalfravega@gmail.com>
2017-05-16 17:29:32 +0200

    Update ejercicio-bucle-informes.py

--- Python_comentado/BUCLES/ejercicio-bucle-informes.py
+++ Python_comentado/BUCLES/ejercicio-bucle-informes.py
@@ -1,13 +1,16 @@
 #coding: utf-8
 
+#Inicialització de les variables.
 anyo = 2010
 salir = False
 
+#Entra al bucle.
 while (salir == False) :
 	
 	print "Informe", anyo
 	
+	#Condició de sortida del bucle.
 	if (anyo == 2016) :
 		salir = True
-    
+    	#Increment de la variable anyo.
 	anyo = anyo + 1
