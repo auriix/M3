daa24b1650e0243a1aa57177ebb7a86c28e0b820
auriix <albavidalfravega@gmail.com>
2017-05-16 17:12:11 +0200

    Update ejercicio-bucle-sumar.py

--- Python_comentado/BUCLES/ejercicio-bucle-sumar.py
+++ Python_comentado/BUCLES/ejercicio-bucle-sumar.py
@@ -1,16 +1,21 @@
 #coding: utf-8
 
+#Inicialització de les variables.
 num = 1
 total = 0
 salir = False
 
+#Entra al bucle.
 while (salir == False) :
   print num
   
+  #Condició de sortida, (fa 5 prints : 1 2 3 4 5)
   if (num == 5) :
     salir = True
-  
+    
+  #Increment de les variables. (1+2+3+4+5)
   total = total + num
   num = num + 1
-  
+
+#Surt per pantalla la suma total: 15
 print "Total: ", total
