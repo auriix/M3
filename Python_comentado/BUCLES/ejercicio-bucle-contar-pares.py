16d37aaeb53c901e94e811c7a91050487b1f2b6f
auriix <albavidalfravega@gmail.com>
2017-05-16 17:45:11 +0200

    Update ejercicio-bucle-contar-pares.py

--- Python_comentado/BUCLES/ejercicio-bucle-contar-pares.py
+++ Python_comentado/BUCLES/ejercicio-bucle-contar-pares.py
@@ -1,14 +1,19 @@
 #coding: utf-8
 
+#S'inicialitzen les variables.
 num = 2
 limite = input ("Escribe el límite: ")
 salir = False
 
+#Entra al bucle.
 while (salir == False):
 	if (num%2 == 0) :
+		#Els numeros surten per pantalla sol si son pars.
   		print num
-	
+		
+	#Condició de sortida.
 	if (num == limite) :
 		salir = True
-  
+		
+  	#Increment de la variable.
 	num = num + 1
