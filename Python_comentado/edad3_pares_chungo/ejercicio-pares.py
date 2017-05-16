e1146600c08f2bd5d1f9c2f6817e73a943b2c1a2
auriix <albavidalfravega@gmail.com>
2017-05-16 16:41:00 +0200

    Update ejercicio-pares.py

--- Python_comentado/edad3_pares_chungo/ejercicio-pares.py
+++ Python_comentado/edad3_pares_chungo/ejercicio-pares.py
@@ -1,7 +1,14 @@
 # coding: utf-8
+
+
+#Esperem a llegir una resposta numèrica, la associem a la variable "numero"
 numero = input ("Inserta un número par : ")
+
+#Condició:
 if numero %2 == 0 :
+	#Si es compleix la condició.
 	print "Qué bonito número par."
 
 else :
+	#Si no es compleix la condició.
 	print "Qué número más vulgar"
