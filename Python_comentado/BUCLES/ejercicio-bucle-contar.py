cdf3771c5506dec9bc4092e4656458d9ecfd20c4
auriix <albavidalfravega@gmail.com>
2017-05-16 17:35:44 +0200

    Update ejercicio-bucle-contar.py

--- Python_comentado/BUCLES/ejercicio-bucle-contar.py
+++ Python_comentado/BUCLES/ejercicio-bucle-contar.py
@@ -1,13 +1,15 @@
 #coding: utf-8
 
+#Inicialització de les variables.
 num = 1
 salir = False
 
+#Entra al bucle.
 while (salir == False) :
 	
 	print num
-	
+	#Condició de sortida del bucle.
 	if (num == 50) :
 		salir = True
-    
+    	#Increment de la variable num.
 	num = num + 1
