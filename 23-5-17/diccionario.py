eae24c5fae8444a4dec7fa2cb02616a09f4205e3
Auriix <auriix@localhost.localdomain>
2017-05-22 22:27:41 +0200

    Ejemplo diccionario

--- 23-5-17/diccionario.py
+++ 23-5-17/diccionario.py
@@ -1,1 +1,12 @@
-cartas={}
+personajes= {"S":"Syndra","L":"Lux","X":"Xayah","A":"Akali"}
+print personajes
+
+print (personajes.get("X"))
+
+#Surt per pantalla i treiem a "A"
+print (personajes.pop("A"))
+
+#Comprovem que "A" estigui al diccionari.
+print ("A" in personajes) #Dona false.
+
+print personajes
