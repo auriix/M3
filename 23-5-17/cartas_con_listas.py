02b16a4020902882ed54a7476ad0211c6c6a6ee0
Auriix <auriix@localhost.localdomain>
2017-05-22 23:00:56 +0200

    cartas con listas

--- 23-5-17/cartas_con_listas.py
+++ 23-5-17/cartas_con_listas.py
@@ -1,25 +1,33 @@
+#coding:utf-8
+
+def my_range(inici,fi,increment):
+	while inici <= fi:
+		yield inici
+		inici = inici + increment
+
+
 palo=["1","2","3","4"]
-num=["2","3","4","5","6","7","8","9","10","11","12","13","14"]
+num=["1","2","3","4","5","6","7","8","9","10","11","12","13"]
 cont= 52
 from random import randint
 
-for palo in range(1,5,1):
-	for numeros in range(1,14,1):
+for palo in my_range(1,4,1):
+	for num in my_range(1,13,1):
   
 		palo=randint(1,4)
-		numeros=randint(1,13)
+		num=randint(1,13)
     
 		if (num <= 10) :
-		carta = num
+			carta = num
 
-    if (num2 == 11) :
-      carta = "J"
+		if (num == 11) :
+			carta = "J"
 
-    if (num2 == 12) :
-      carta = "Q"
+		if (num == 12) :
+			carta = "Q"
 
-    if (num2 == 13) :
-      carta = "K"
+		if (num == 13) :
+			carta = "K"
 			
 		if(palo == 1):
 			palo= "D"
@@ -33,6 +41,6 @@ for palo in range(1,5,1):
 		if(palo == 4):
 			palo= "P"
 				
-		print numeros, palo
+		print num, palo
 		
 cont= cont- 1
