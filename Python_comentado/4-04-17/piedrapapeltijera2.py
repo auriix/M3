70063a9b6c780cd3597230c6d90bf75c0f653bdb
auriix <albavidalfravega@gmail.com>
2017-05-23 10:42:58 +0200

    Update piedrapapeltijera2.py

--- Python_comentado/4-04-17/piedrapapeltijera2.py
+++ Python_comentado/4-04-17/piedrapapeltijera2.py
@@ -1,10 +1,11 @@
 #coding: utf-8
-
+#Inicialitza variables.
 num = 31
-
 salir = False
 
-while salir==False :
+#Inici del bucle.
+while salir==False : #Condició d'entrada.
+	#Torns de jugada del Jugador 1:
 	
 	if num%7 == 0 or num%7 == 1 :
 		j1 = "tijera"
@@ -15,7 +16,7 @@ while salir==False :
 	elif num%7 == 4 or num%7 == 5 :
 		j1 = "papel"
 		
-		
+	#Torns de jugada del Jugador 2:	
 	if num%5 == 0 or num%5 == 1 or num%5 == 2 :
 		j2 = "papel"
 	
@@ -25,18 +26,20 @@ while salir==False :
 	elif num%5 == 4 :
 		j2 = "piedra"
 	
+	
+	#Si tenen el mateix resultat:
 	if (j1 == j2) :
 		print num, "Empate"
-	
+	#Regles del joc.
 	elif (j1 == "piedra" and j2 == "papel") or (j1 == "papel" and j2 == "tijera") or (j1 == "tijera" and j2 == "piedra") :
 		print num, "Gana el Jugador 2."
 		
 	else :
 		print num, "Gana el Jugador 1."
 	
-
+#Condició de sortida:
 	if num == 57 :
 		
 		salir = True
-	
+#Increment de la variable.
 	num = num + 1
