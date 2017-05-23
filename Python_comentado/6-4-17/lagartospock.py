6b3b3a29a6056308dafb29a57e3a99a7803d7f84
auriix <albavidalfravega@gmail.com>
2017-05-23 10:19:43 +0200

    Update lagartospock.py

--- Python_comentado/6-4-17/lagartospock.py
+++ Python_comentado/6-4-17/lagartospock.py
@@ -1,13 +1,15 @@
 #coding: utf-8
-
+#Imports
 import os
-
 os.system ('clear')
-
 from random import randint
 
+#Jugador
+#Esperem a llegir el teclat:
 j1 = raw_input ("Jugador 1 > Elige piedra, papel, tijera, lagarto o spock :")
 
+#Màquina
+#Generem un número random del 1 al 5.
 aleatori = randint (1,5)
 
 if (aleatori == 1) :
@@ -29,7 +31,7 @@ print "La máquina elige:", j2
 
 if (j1 == j2 ) :
 	print "Empate"
-else :
+else : #Regles del joc.
 	if (j1 == "piedra" and (j2 == "lagarto" or j2 == "tijera")) or (j1 == "papel" and (j2 == "piedra" or j2 == "spock"))or (j1 == "tijera" and (j2 == "papel" or j2 == "lagarto")) or (j1 == "lagarto" and (j2 == "papel" or j2 == "spock")) or (j1 == "spock" and (j2 == "piedra" or j2 == "tijera")) :
 		print "Gana el Jugador 1."
 	else :
