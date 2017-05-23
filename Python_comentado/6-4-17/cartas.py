73256ec1650a1074d2d0e9e92b3f43065ea66650
auriix <albavidalfravega@gmail.com>
2017-05-23 10:16:11 +0200

    Update cartas.py

--- Python_comentado/6-4-17/cartas.py
+++ Python_comentado/6-4-17/cartas.py
@@ -1,34 +1,48 @@
 #coding: utf-8
 
+#Importem la llibrería.
 from random import randint 
 
+#Generem número random del 1 al 4.
 aleatori = randint (1,4)
+
+#Si el numero generat es 1:
 if (aleatori == 1) :
 	palo = "de picas"
-	
+#Si el numero generat es 2:	
 if (aleatori == 2) :
 	palo = "de diamantes"
-
+#Si el numero generat es 3:
 if (aleatori == 3) :
 	palo = "de treboles"
-	
+#Si el numero generat es 4:	
 if (aleatori == 4) :
 	palo = "de corazones"
-	
+
+
+#Generem número random del 1 al 13.
 num = randint (1,13)
+
+#Si el numero generat es menor o igual a 10:
 if (num <= 10) :
 	carta = num
 
+#Si el numero generat es 11:
 if (num == 11) :
 	carta = "J"
 
+	
+#Si el numero generat es 12:
 if (num == 12) :
 	carta = "Q"
 
+	
+#Si el numero generat es 13:
 if (num == 13) :
 	carta = "K"
 
 
+#Fem el mateix per l'altre jugador.
 aleatori2 = randint (1,4)
 if (aleatori2 == 1) :
 	palo2 = "de picas"
@@ -55,10 +69,11 @@ if (num2 == 12) :
 if (num2 == 13) :
 	carta2 = "K"
 
-
+#El resultat de cada jugador:
 print "Màquina:", carta, palo
 print "Tú:", carta2, palo2
 
+#Si el número generat de la primera carta es major que la segona: ¿Qui guanya?
 if (num > num2) :
 	print "La màquina guanya!!"
 else :
