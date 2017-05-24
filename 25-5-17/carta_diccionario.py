8a87f7f6e68912a7485cc5f00699666985c8a894
auriix <albavidalfravega@gmail.com>
2017-05-24 17:49:17 +0200

    Update carta_diccionario.py

--- 25-5-17/carta_diccionario.py
+++ 25-5-17/carta_diccionario.py
@@ -1,35 +1,26 @@
 #coding: utf-8
-
+#Imports
 import os
 os.system('clear')
+import random
 
-diccionari = { "1" :'1D',"2":'2D',"3":'3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD',
-	      '1T', '2T', '3T', '4T', '5T', '6T', '7T', '8T', '9T', '10T', 'JT', 'QT', 'KT',
-	      '1P', '2P', '3P', '4P', '5P', '6P', '7P', '8P', '9P', '10P', 'JP', 'QP', 'KP',
-	      '1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC'}
+cartas = {'1': , '2': ,'3': , '4': ,'5': , '6': ,'7': , '8': ,'9': , '10': ,'11': , '12': ,'13': , '14': }
+palo = = {'1': , '2': ,'3': , '4': }
 
-from random import randint 
-salir = False
+maquina = random.choice(cartas.keys()) + random.choice(palo.keys())
+jugador = random.choice(cartas.keys()) + random.choice(palo.keys())
 
-while (salir==False) :
-	
-	random.choice(diccionari.keys())
-	print "Màquina":
-	diccionari.pop (random.choice)	
-	
+print "Màquina: " + maquina 
+print "Jugador: " + jugador
 
-
-	print "Màquina:", carta, palo
-	print "Tú:", carta2, palo2
-
-	if (num == num2) :
-		print "Empat"
+if (num == num2) :
+	print "Empat"
+else :
+	if (num > num2) :
+		print "Guanya la màquina!!"
 	else :
-		if (num > num2) :
-			print "Guanya la màquina!!"
-		else :
-			print "Has guanyat!!"
+		print "Has guanyat!!"
 		
 
-	if (num <> num2) :
-salir = True
+if (num <> num2) :
+	salir = True
