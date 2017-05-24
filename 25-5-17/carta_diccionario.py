85281d40633dc448b7a26443709d40ae4cfe2b3e
Auriix <auriix@localhost.localdomain>
2017-05-24 18:30:45 +0200

    v1

--- 25-5-17/carta_diccionario.py
+++ 25-5-17/carta_diccionario.py
@@ -8,16 +8,41 @@ os.system('clear')
 
 #Variables
 salir = False
-cartas = {'1': , '2': ,'3': , '4': ,'5': , '6': ,'7': , '8': ,'9': , '10': ,'11': , '12': ,'13': , '14': }
-palo = = {'1': , '2': ,'3': , '4': }
+cartas = {'2': 2 ,'3': 3 , '4': 4 ,'5': 5 , '6': 6 ,'7': 7  , '8': 8 ,'9': 9 , '10': 10 , 'J': 11 , 'Q': 12,'K': 13, 'AS': 14 }
+palo = {'Diamantes':"1", 'Picas':"2", 'Tréboles':"3", 'Corazones':"4"}
 
-while (salir==False) do
+while (salir==False) :
 
 	maquina = random.choice(cartas.keys()) 
 	jugador = random.choice(cartas.keys()) 
+	
+	print "Màquina: ", maquina, random.choice(palo.keys())
+	print "Jugador: ", jugador, random.choice(palo.keys())
+	
+	
+	if (maquina == "J") :
+		maquina = 11
 
-	print "Màquina: " + maquina + random.choice(palo.keys())
-	print "Jugador: " + jugador + random.choice(palo.keys())
+	if (maquina == "Q") :
+		maquina = 12
+
+	if (maquina == "K") :
+		maquina = 13
+	
+	if (maquina == "AS") :
+		maquina = 14
+	
+	if (jugador == "J") :
+		jugador = 11
+
+	if (jugador == "Q") :
+		jugador = 12
+
+	if (jugador == "K") :
+		jugador = 13
+	
+	if (jugador == "AS") :
+		jugador = 14
 
 	if (maquina == jugador) :
 		print "Empat"
