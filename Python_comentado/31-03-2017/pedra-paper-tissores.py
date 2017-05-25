0caa69cca053c9809c5889c5866968d071b64fd2
auriix <albavidalfravega@gmail.com>
2017-05-25 13:25:59 +0200

    Update pedra-paper-tissores.py

--- Python_comentado/31-03-2017/pedra-paper-tissores.py
+++ Python_comentado/31-03-2017/pedra-paper-tissores.py
@@ -1,11 +1,12 @@
 #coding: utf-8
-
+#Llegim l'elecció de cada jugador.
 j1 = raw_input ("Jugador 1 Elige > piedra, papel o tijera: ")
 j2 = raw_input ("Jugador 2 Elige > piedra, papel o tijera: ")
 
+#Els comparem:
 if (j1 == j2) :
 	print "Empate"
-else :
+else : #Regles del joc.
 	if (j1 == "piedra" and j2 == "papel") or (j1 == "papel" and j2 == "tijera") or (j1 == "tijeras" and j2 == "piedra") :
 		print "Gana el jugador 2."
 	else :
