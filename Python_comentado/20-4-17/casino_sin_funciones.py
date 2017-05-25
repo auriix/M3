8c2b66cccc596b697877e87baff008b19b2385e3
auriix <albavidalfravega@gmail.com>
2017-05-25 13:39:43 +0200

    Update casino_sin_funciones.py

--- Python_comentado/20-4-17/casino_sin_funciones.py
+++ Python_comentado/20-4-17/casino_sin_funciones.py
@@ -1,31 +1,40 @@
 # coding:utf-8
-
-
+#Imports
 import os
 os.system('clear')
-
 from random import randint 
+
+#Variables
 saldo = 100
 apuesta = 0
 salir_apuesta = False 	
+
+#Surt per pantalla el teu saldo.
 print "Tu saldo inicial:", saldo
-		
+
+#Condició d'entrada al bucle.		
 while (salir_apuesta == False) :
+	#Elegim la cantitat a apostar.
 	apuesta = input ("Cantidad a apostar:")
+	
 	
 	if (apuesta < 10 and apuesta >= 1) :
 		print "El mínimo a apostar es 10"
 		print ""
+		
+	#Condició de sortida del bucle.
 	if (apuesta == -1) :
 		print "Adiós"
 		salir_apuesta = True
-	
+		
+	#Si la cantitat introduïda per apostar es correcte establim la variable "salir" que ens servirà per entrar al següent bucle.
 	if (apuesta > 10 and apuesta < saldo) :
 		salir = False
 		
-
+		#Entrada del segon bucle.
 		while (salir==False) :
-
+			
+			#Nombre aleatori per el pal de cada carta.
 			aleatori = randint (1,4)
 			if (aleatori == 1) :
 				palo = "de picas"
@@ -38,7 +47,9 @@ while (salir_apuesta == False) :
 				
 			if (aleatori == 4) :
 				palo = "de corazones"
-				
+			
+
+			#Nombre aleatori per el número de cada carta.
 			num = randint (2,14)
 			if (num <= 10) :
 				carta = num
@@ -52,10 +63,10 @@ while (salir_apuesta == False) :
 			if (num == 13) :
 				carta = "K"
 			
-			if (num == 14) :
-				carta = "A"
+			if (num == 14) : 
+				carta = "AS"
 
-
+			#Fem el mateix per la banca.
 			aleatori2 = randint (1,4)
 			if (aleatori2 == 1) :
 				palo2 = "de picas"
@@ -85,28 +96,33 @@ while (salir_apuesta == False) :
 			if (num2 == 14) :
 				carta2 = "A"
 			
+			#Resultats:
 			print ""
 			print "Banca:", carta, palo
 			print "Yo:", carta2, palo2
 			print ""
 
-
+			#Qui guanya:
 			if ( num > num2 or num == num2 ) :
 				print "Gana la banca."
+				#Si la banca guanya el teu saldo baixa.
 				saldo = (saldo - apuesta)
 				print "Tu saldo:", saldo
+				#Surt del bucle de les jugades.
 				salir = True
 				
 			if ( num < num2 ) :
 				print "Ganas tú."
+				#Si tu guanyes el teu saldo puja.
 				saldo = (saldo + (apuesta * 2))
 				print "Tu saldo:", saldo
+				#Surt del bucle de les jugades.
 				salir = True
 	
+	#Condició de sortida del primer bucle.
 	if (salir == True ) :
 		
-			
-		salir_apuesta = True
+			salir_apuesta = True
 
 
 			
