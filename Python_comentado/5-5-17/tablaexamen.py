472252559b2820e805e62c38ff88a88345bd6781
auriix <albavidalfravega@gmail.com>
2017-05-25 13:29:57 +0200

    Update tablaexamen.py

--- Python_comentado/5-5-17/tablaexamen.py
+++ Python_comentado/5-5-17/tablaexamen.py
@@ -1,3 +1,17 @@
+#coding: utf8
+#Definim la funció my_range.
+def my_range(inici, fi, increment):
+    while inici <= fi:
+        #Retorna l'element actual del rang (llista)
+        yield inici
+        inici = inici + increment
+
+# - A B C
+# 1 
+# 2
+# 3
+# 4
+
 for fil in my_range(1,5,1):
   for col in my_range(1,4,1):
     if (fil==1):
