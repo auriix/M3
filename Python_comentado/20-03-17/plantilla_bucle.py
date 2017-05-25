338aa05e7ae8bcfb5af737ef7576e5c59faf0b78
auriix <albavidalfravega@gmail.com>
2017-05-25 13:40:59 +0200

    Update plantilla_bucle.py

--- Python_comentado/20-03-17/plantilla_bucle.py
+++ Python_comentado/20-03-17/plantilla_bucle.py
@@ -1,13 +1,13 @@
 #coding: utf-8
-
+#Variables
 salir = False
-
+#Condició d'entrada al bucle.
 while (salir == False) :
-	
+	#Print
 	print ("Hola")
-	
+	#Read
 	tecla = raw_input ("Sal con s o S:")
-	
+	#Condició de sortida del bucle.
 	if (tecla == "S" or tecla == "s" ) :
 		print ("Adiós")
 		salir = True
