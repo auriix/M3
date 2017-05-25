ce0a4ce8d8da152d4cd60ee087842ddb20d60e02
ism26539567 <ism26539567@a12.informatica.escoladeltreball.org>
2017-05-25 13:59:02 +0200

    v1

--- 26-5-17/permisosothers.py
+++ 26-5-17/permisosothers.py
@@ -1,1 +1,25 @@
+# coding: utf-8
 
+# Imports
+import os
+import stat
+
+path_to_explore = raw_input('Introduzca la ruta a explorar: ')
+
+
+for root, dirs, files in os.walk(path_to_explore):
+	for d in dirs:
+		permisosd = oct(stat.S_IMODE (os.stat(path_to_explore).st_mode))
+		
+		if (permisosd[-1] != 0):
+			print "Hi ha un error de seguretat."
+		else:
+			print "No hi han errors de seguretat."
+			
+	for f in files:
+		permisosf = oct(stat.S_IMODE (os.stat(path_to_explore).st_mode))
+		 
+		if (permisosf[-1] != 0):
+			print "Hi ha un error de seguretat."
+		else:
+			print "No hi han errors de seguretat."
