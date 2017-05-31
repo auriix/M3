fc4d75f254c366fb7fac1c44c51e3d5609ef4d8c
auriix <albavidalfravega@gmail.com>
2017-05-31 22:37:16 +0200

    Update ultacceso1gb.py

--- 1-5-17/ultacceso1gb.py
+++ 1-5-17/ultacceso1gb.py
@@ -1,1 +1,15 @@
+# coding:utf-8
+import os
 
+path_to_explore="./"
+totalsize=0
+        
+for root, dirs, files in os.walk(path_to_explore):
+    for x in files:
+        name_path=os.path.join(root, x)
+        #Mostrem el que ocupa de cada arxiu.
+        tamany=os.stat(name_path).st_size
+        
+
+if (tamany>=2**30):
+  print "Arxius que ocupen 1GB o mès:\n", name_path
